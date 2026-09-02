f242b5c03cb6874c09dfdd169f936ed7493d161b
Prajwal Jadhav <jadhavprajwal2307@gmail.com>
2026-09-02 23:17:54 +0530

    star_12

--- star_11.cpp
+++ star_11.cpp
@@ -11,9 +11,6 @@ int main(){
         for(col=1;col<=2*row-1;col++){
             cout << "* ";
         }
-        //  for(col=1;col<=row-1;col++){
-        //     cout << "* ";
-        // }
         cout << endl;
     }
 
