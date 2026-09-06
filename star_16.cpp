8eca9b7bd8ecaad48211f9bf878bf62342033c5f
Prajwal Jadhav <jadhavprajwal2307@gmail.com>
2026-09-06 20:08:21 +0530

    star17 and while

--- star_16.cpp
+++ star_16.cpp
@@ -1,35 +1,33 @@
 #include<iostream>
 using namespace std;
 int main(){
-
-    int row , col, n;
-    cout << "Enter any number : ";
+    int n, row , col;
+    cout<<"Enter the number of rows: ";
     cin >> n;
 
-    for(row=1;row<n;row++){
+    for(row=1;row<=n;row++){
         for(col=1;col<=row;col++){
             cout << "* ";
         }
-        for(col=1;col<=(2*n-2*row);col++){
+        for(col=1;col<=2*n-2*row;col++){
             cout << "  ";
         }
         for(col=1;col<=row;col++){
             cout << "* ";
         }
-        
         cout << endl;
     }
-    for(row=n;row>=1;row--){
+
+    for(row=n-1;row>=1;row--){
         for(col=1;col<=row;col++){
             cout << "* ";
         }
-        for(col=1;col<=(2*n-2*row);col++){
+        for(col=1;col<=2*n-2*row;col++){
             cout << "  ";
         }
         for(col=1;col<=row;col++){
             cout << "* ";
         }
-        
         cout << endl;
     }
 }
