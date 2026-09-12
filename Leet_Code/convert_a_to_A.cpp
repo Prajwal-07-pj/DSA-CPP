8265e4c8dbc0264a6cabde73121578c23f47726c
Prajwal Jadhav <jadhavprajwal2307@gmail.com>
2026-09-12 16:39:51 +0530

    convert_a_to_A

---
+++ Leet_Code/convert_a_to_A.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+char convert(char ch){
+    char ans = ch - 'a' + 'A';
+    return ans;
+}
+
+int main(){
+    char ch;
+    cout << "Enter any character : ";
+    cin >> ch;
+    cout << convert(ch);
+}
