5598ce548fc29334b31551be950475dc163de453
Prajwal Jadhav <jadhavprajwal2307@gmail.com>
2026-09-14 14:08:22 +0530

    rotate by one

---
+++ Leet_Code/rotate_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    int temp = arr[5-1];
+
+    for(int i=5-2;i>=0;i--){
+        arr[i+1] = arr[i];
+    }
+       arr[0] = temp;   
+    
+    for(int i=0;i<5;i++){
+        cout << arr[i];
+    }
+    
+}
