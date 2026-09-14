6be39a94dd230e6e6a29c3bf8898a46d04834449
Prajwal Jadhav <jadhavprajwal2307@gmail.com>
2026-09-14 11:48:01 +0530

    fibonacci

---
+++ Leet_Code/fibonacci.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the n :";
+    cin >> n;
+
+    int a = 0;
+    int b  = 1;
+
+
+    for(int i=1;i<=n;i++){
+       cout << a << " ";
+    
+       int c = a+b;
+       a = b;
+       b = c;
+    }
+}
