42cf0af0b647ee0ee80443dc22291c995c836efa
Prajwal Jadhav <jadhavprajwal2307@gmail.com>
2026-09-15 23:53:26 +0530

    bubble sort 2

--- Leet_Code/bubble_sort.cpp
+++ Leet_Code/bubble_sort.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[5] = {5,4,3,2,1};
+    int n;
+    cout << "Enter the size of array : ";
+    cin >>n;
+    int arr[n];
+
+    cout<<"Enter the values" << endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
 
     for(int i=0;i<5;i++){
         for(int j=0;j<5-i;j++){
