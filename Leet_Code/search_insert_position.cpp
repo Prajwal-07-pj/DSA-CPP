a5306d4a796547e74be25833059e19feaa8d82b9
Prajwal Jadhav <jadhavprajwal2307@gmail.com>
2026-09-19 19:06:13 +0530

    search insert position

---
+++ Leet_Code/search_insert_position.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={2,4,6,8,9};
+    int key = 5;
+    int i=0,j=4,mid;
+    int index = 4;
+
+    while(i<=j){
+        mid=i+(j-i)/2;
+        if(arr[mid]==key){
+            index = mid;
+            break;
+        }
+        else if(arr[mid]<key){
+            i = mid + 1;
+        }else{
+            index = mid;
+            j  = mid-1;
+        }
+    }
+
+    cout << index;
+}
