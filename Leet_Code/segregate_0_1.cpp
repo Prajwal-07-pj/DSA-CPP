1902e62ce8a45cb80e5028cadff5bfb33ba153ad
Prajwal Jadhav <jadhavprajwal2307@gmail.com>
2026-09-26 10:43:49 +0530

    segregate 0 1

---
+++ Leet_Code/segregate_0_1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[6] = {1,0,1,0,1,0};
+    int n = 6;
+    int count0 = 0 , count1=0;
+
+    for(int i=0;i<n;i++){
+        if(arr[i] == 0){
+            count0++;
+        }else{
+            count1++;
+        }
+    }
+
+    for(int i=0;i<count0;i++){
+        arr[i] = 0;
+    }
+
+    for(int i=count0;i<n;i++){
+        arr[i] = 1;
+    }
+
+    for(int i=0;i<n;i++){
+        cout << arr[i];
+    }
+}
